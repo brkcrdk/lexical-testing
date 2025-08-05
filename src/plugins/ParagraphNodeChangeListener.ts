126b9afa2fd0f01e85ac0ee52eaa5c3a346c7973
Burak <burak@90pixel.com>
2025-08-05 22:05:25 +0300

    Refactor ParagraphNodeChangeListener to track focus changes more accurately by managing previous focused node state, improving focus state updates for paragraph nodes.

--- src/plugins/ParagraphNodeChangeListener.ts
+++ src/plugins/ParagraphNodeChangeListener.ts
@@ -1,51 +1,52 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useEffect } from "react";
-import { $isCustomParagraphNode } from "../nodes/CustomParagraphNode";
 import {
-  $getRoot,
+  $isCustomParagraphNode,
+  CustomParagraphNode,
+} from "../nodes/CustomParagraphNode";
+import {
+  $getNodeByKey,
   $getSelection,
   $isRangeSelection,
   COMMAND_PRIORITY_LOW,
   SELECTION_CHANGE_COMMAND,
 } from "lexical";
-import { $getNearestBlockElementAncestorOrThrow } from "@lexical/utils";
 
 function ParagraphNodeChangeListener() {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
+    let prevFocusedNodeKey: null | string = null;
+
     const unregisterTest = editor.registerCommand(
       SELECTION_CHANGE_COMMAND,
       () => {
         const selection = $getSelection();
 
         if ($isRangeSelection(selection)) {
-          const caretNode = selection.anchor.getNode();
           const anchorNode = selection.anchor.getNode();
           const isParagraphNode = $isCustomParagraphNode(anchorNode);
+
           if (isParagraphNode) {
-            const root = $getRoot();
+            const currentFocusedNodeKey = anchorNode.getKey();
 
-            const paragraphNodes = root.getChildren().filter((node) => {
-              return $isCustomParagraphNode(node);
-            });
-            const selectedNode =
-              $getNearestBlockElementAncestorOrThrow(caretNode);
+            if (prevFocusedNodeKey !== currentFocusedNodeKey) {
+              if (prevFocusedNodeKey) {
+                const prevFocusedNode =
+                  $getNodeByKey<CustomParagraphNode>(prevFocusedNodeKey);
 
-            paragraphNodes.forEach((node) => {
-              // Bu kontrolü filtre yaparken yapalım ve ts uygunluğunu orada ayarlayalım.
-              if (node.isEmpty()) return;
-              if (node.getKey() === selectedNode.getKey()) {
-                node.updateFocusTest(true);
-              } else {
-                node.updateFocusTest(false);
+                if (prevFocusedNode) {
+                  prevFocusedNode.updateFocusTest(false);
+                }
               }
-            });
+
+              anchorNode.updateFocusTest(true);
+            }
+            prevFocusedNodeKey = currentFocusedNodeKey;
 
             return true;
           }
         }
-
         return false;
       },
       COMMAND_PRIORITY_LOW
