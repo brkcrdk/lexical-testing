bc40a9762da39aad75d433d3100cc2927005168f
Burak <burak@90pixel.com>
2025-08-10 23:21:49 +0300

    Implement insertBlock function in DraggableBlockPlugin to allow insertion of paragraph nodes. Update button click handlers for improved functionality and user interaction.

--- src/plugins/DraggableBlockPlugin.tsx
+++ src/plugins/DraggableBlockPlugin.tsx
@@ -1,17 +1,38 @@
 import { DraggableBlockPlugin_EXPERIMENTAL } from "@lexical/react/LexicalDraggableBlockPlugin";
 import { useRef, useState } from "react";
 import { Plus, GripVertical } from "lucide-react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { $createParagraphNode, $getNearestNodeFromDOMNode } from "lexical";
 
 interface Props {
   anchorElem: HTMLElement | undefined;
 }
 
 function DraggableBlockPlugin({ anchorElem= document.body }: Props) {
+  const [editor] = useLexicalComposerContext();
   const menuRef = useRef<HTMLDivElement>(null);
   const targetLineRef = useRef<HTMLDivElement>(null);
   const [draggableElement, setDraggableElement] = useState<HTMLElement | null>(
     null
   );
+
+  function insertBlock(){
+    if (!draggableElement || !editor) {
+      return;
+    }
+
+    editor.update(() => {
+      const node = $getNearestNodeFromDOMNode(draggableElement);
+      if (!node) {
+        return;
+      }
+
+      const pNode = $createParagraphNode();
+      node.insertAfter(pNode);
+      pNode.select();
+    });
+  }
+
   return (
     <DraggableBlockPlugin_EXPERIMENTAL
       anchorElem={anchorElem}
@@ -21,12 +42,15 @@ function DraggableBlockPlugin({ anchorElem= document.body }: Props) {
         <div
           data-draggable-block-menu
           className="rounded-sm opacity-0 absolute left-0 top-0 will-change-transform flex gap-0.5 p-0.5"
-          ref={menuRef}>
-          <button className="">
-            <Plus size={12} />
+          ref={menuRef}
+        >
+          <button className="" onClick={insertBlock}>
+            <Plus size={14} />
           </button>
-          <button className="cursor-grab active:cursor-grabbing">
-            <GripVertical size={12} />
+          <button className="cursor-grab active:cursor-grabbing" onClick={()=>{
+            console.log('xx')
+          }}>
+            <GripVertical size={14} />
           </button>
         </div>
       }
