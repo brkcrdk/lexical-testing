008c8c509ad624cb4deccde743403dda6e1328da
Burak <burak@90pixel.com>
2025-08-11 21:43:07 +0300

    Refactor NodeList component by integrating option selection logic directly, removing the OptionItem component for improved simplicity and performance. Update event handling for option selection and highlighting.

--- src/plugins/TypeaheadNodeSelection/NodeList.tsx
+++ src/plugins/TypeaheadNodeSelection/NodeList.tsx
@@ -1,4 +1,4 @@
-import OptionItem from "./OptionItem";
+
 import type { CustomNodeOption } from "./useGenerateNodeOptions";
 
 interface Props{
@@ -8,17 +8,29 @@ interface Props{
   setHighlightedIndex: (index: number) => void;
 }
 
-function NodeList({ options,selectedIndex }:Props) {
+function NodeList({ options,selectedIndex,selectOptionAndCleanUp,setHighlightedIndex }:Props) {
   return (
-      <ul className="bg-white shadow-md rounded-md p-2 relative w-2xs ">
-        {options.map((option, index) => (
-          <OptionItem
-            key={option.key}
-            option={option}
-            isSelected={selectedIndex === index}
-          />
-        ))}
-      </ul>
+    <ul className="bg-white shadow-md rounded-md p-2 relative w-2xs ">
+      {options.map((option, index) => (
+        <li
+          className="flex items-center text-black justify-between aria-selected:bg-black/60 aria-selected:text-white p-1 rounded-md relative"
+          aria-selected={selectedIndex === index}
+          data-option={option.key}
+          role="option"
+          ref={option.setRefElement}
+          onClick={() => {
+            setHighlightedIndex(index);
+            selectOptionAndCleanUp(option);
+          }}
+          onPointerEnter={() => {
+            setHighlightedIndex(index);
+          }}
+          >
+          <span className="text-sm">{option.__nodeOption.title}</span>
+          <span className="text-xs">{option.__nodeOption.triggerPattern}</span>
+        </li>
+      ))}
+    </ul>
   );
 }
 
