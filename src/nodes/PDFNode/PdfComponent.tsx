33ec5e637b8907798bf15cd37a296894e0eab5d3
Burak <burak@90pixel.com>
2025-08-18 18:11:36 +0300

    Enhance PdfComponent for improved PDF rendering; update MediaNodeWrapper styles and add test2.pdf for testing purposes.

--- src/nodes/PDFNode/PdfComponent.tsx
+++ src/nodes/PDFNode/PdfComponent.tsx
@@ -1,29 +1,43 @@
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+import workerSrc from "pdfjs-dist/build/pdf.worker.min.mjs?url";
+
+pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+
+// import testPdf from "./test.pdf";
+import testPdf from "./test2.pdf";
+import MediaNodeWrapper from "../../components/MediaNodeWrapper";
 
 function PdfComponent() {
   const [numPages, setNumPages] = useState<number | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const [scale, setScale] = useState(1);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
     setNumPages(numPages);
   }
 
   return (
-    <div>
-      <Document
-        file="./test.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+    <div className="relativeoverflow-hidden">
+      <Document file={testPdf} onLoadSuccess={onDocumentLoadSuccess}  className='aspect-video w-fit'>
+        <Page
+          pageNumber={pageNumber}
+          scale={1}
+          className="border-1 border-dashed border-blue-600"
+          loading="Sayfa yükleniyor..."
+          error="Sayfa yüklenirken hata oluştu."
+        />
       </Document>
-      <p>
+      {/* <p>
         Page {pageNumber} of {numPages}
-      </p>
+      </p> */}
+      <footer>
+            <button></button>
+
+      </footer>
     </div>
   );
 }
